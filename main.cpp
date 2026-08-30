dc3a3895ec3d6d47caee020c33d39e7b6b7edb85
Zhevaho Oleksandr <marakonec@gmail.com>
2026-08-30 07:00:00 +0300

    Додано меню з переліком операцій

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
 ﻿#include <iostream>
+#include <string>
 #include <windows.h>
+
+void printMenu()
+{
+	std::cout << "\n Конвертер римських чисел \n";
+	std::cout << "1 - число -> римський запис\n";
+	std::cout << "2 - римський запис -> число\n";
+	std::cout << "0 - вихід\n";
+	std::cout << "Ваш вибір: ";
+}
 
 int main()
 {
 	SetConsoleOutputCP(1251);
-	std::cout << "Конвертер римських чисел\n";
+
+	std::string command;
+	while (true)
+	{
+		printMenu();
+		if (!(std::cin >> command))
+		{
+			break;
+		}
+		if (command == "0")
+		{
+			break;
+		}
+		std::cout << "Цей пункт ще не працює.\n";
+	}
+
+	std::cout << "До побачення.\n";
 	return 0;
 }
